292aceb8500a41840fd9f4f931668783d258073a
ykkssyaa <mr.ender.03@mail.ru>
2023-10-24 12:37:33 +0300

    Graceful Shutdown

--- app.go
+++ app.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
 	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	config "testTask/config"
 	"testTask/internal/gateway"
 	"testTask/internal/server"
@@ -36,11 +41,28 @@ func main() {
 	logger.Info.Print("Creating server.")
 
 	port := viper.GetString("PORT")
-
 	srv := server.NewHttpServer(services, logger, ":"+port)
 
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			logger.Err.Fatalf("error occured while running http server: \"%s\" \n", err.Error())
+		}
+	}()
+
 	logger.Info.Print("Starting the server on port: " + port + "\n\n")
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	<-quit
 
-	logger.Err.Fatalf(srv.ListenAndServe().Error())
+	logger.Info.Println("Server Shutting Down.")
 
+	if err = srv.Shutdown(context.Background()); err != nil {
+		logger.Err.Fatalf("error occured while server shutting down: \"%s\" \n", err.Error())
+	}
+
+	logger.Info.Println("DB connection closing.")
+
+	if err := db.Close(); err != nil {
+		logger.Err.Fatalf("error occured on db connection close: \"%s\" \n", err.Error())
+	}
 }
